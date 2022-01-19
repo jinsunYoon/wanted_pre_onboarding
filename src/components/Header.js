c2a54790b1f6c734630df9e1c52b7e07853618e2
jinsunYoon <yks9013@naver.com>
2022-01-19 09:39:33 +0900

    menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,47 @@ import styled from 'styled-components'
 
 const Header = (props) => {
     return (
-        <>
-            <p>안녕하세요~~~ 헤더입니다</p>
-        </>
+        <Flex>
+            <div className="nav">
+                <img src="https://static.wanted.co.kr/images/icon-menu.png" alt="hamberger menu" />
+                <a
+                    href="/"
+                    class="MainBar_MainBar_logo__bGymr"
+                    aria-label="homeLink"
+                    data-attribute-id="gnb"
+                    data-gnb-kind="home"
+                >
+                    <i class="icon-logo_new"></i>
+                </a>
+            </div>
+        </Flex>
     )
 }
+
+const Flex = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+    .nav {
+        width: 1060px;
+        height: 50px;
+        background-color: #ddd;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        img {
+            width: 17px;
+            height: 14px;
+            object-fit: contain;
+            padding-right: 1rem;
+        }
+
+        a {
+            font-family: 'icomoon';
+        }
+    }
+`
 
 export default Header
